99a32ad6ff00003a58747b8a19c0fa0f70b33a2b
Kaseamsan Keawjeen <kj.kaseamsankeawjeen@gmail.com>
2025-01-03 11:37:52 +0700

    add sqlite3 in api webhook and createCheckout

--- backend/index.js
+++ backend/index.js
@@ -4,66 +4,118 @@ const cors = require("cors");
 const mySql = require("mysql");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const { v4: uuidv4 } = require("uuid");
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database("./teststripe.db");
+
+db.serialize(() => {
+  db.run(
+    "CREATE TABLE IF NOT EXISTS transactionPayment(id INT, fullName TEXT, orderID INT, status TEXT, paymentIntent TEXT)"
+  );
+});
 
 const app = express();
 const PORT = 3000;
 
+const packageQrCode = {
+  ESSENTIAL: {
+    price: 5.99,
+    name: "Essential pakage OrcaCode",
+  },
+  PROFESSIONAL: {
+    price: 10.99,
+    name: "Professional pakage OrcaCode",
+  },
+  ULTIMATE: {
+    price: 30.99,
+    name: "Ultimate pakage OrcaCode",
+  },
+};
+
+const checkPackage = (name) => {
+  if (name.toUpperCase() == "ESSENTIAL") {
+    return packageQrCode.ESSENTIAL;
+  } else if (name.toUpperCase() == "PROFESSIONAL") {
+    return packageQrCode.PROFESSIONAL;
+  } else if (name.toUpperCase() == "ULTIMATE") {
+    return packageQrCode.ULTIMATE;
+  } else {
+    throw { message: "package not match" };
+  }
+};
+
 app.use(cors());
 
-const connection = mySql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "testStripe",
-});
+// const connection = mySql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "testStripe",
+// });
 
 app.get("/test", (req, res) => {
   res.send("test success.");
 });
 
 app.post("/api/checkout", express.json(), async (req, res) => {
-  const { product, user } = req.body;
+  try {
+    const { product, user } = req.body;
 
-  const orderId = uuidv4();
+    const transactionID = uuidv4();
+    const name = user.name;
 
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: [
-      {
-        price_data: {
-          currency: "thb",
-          product_data: {
-            name: product.name,
-            images: [
-              "https://static1.cbrimages.com/wordpress/wp-content/uploads/2023/05/minato-s-kunai-naruto.jpg",
-            ],
+    const selectPackage = checkPackage(product.name);
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: selectPackage.name,
+              images: [
+                "https://static1.cbrimages.com/wordpress/wp-content/uploads/2023/05/minato-s-kunai-naruto.jpg",
+              ],
+            },
+            unit_amount: selectPackage.price * 100,
           },
-          unit_amount: product.price * 100,
+          quantity: 1,
         },
-        quantity: product.quantity,
-      },
-    ],
-    mode: "payment",
-    success_url: `http://localhost:3333/success.html?orderId=${orderId}`,
-    cancel_url: `http://localhost:3333/cancel.html`,
-  });
+      ],
+      mode: "payment",
+      success_url: `http://localhost:3333/success.html?transactionID=${transactionID}`,
+      cancel_url: `http://localhost:3333/cancel.html`,
+    });
 
-  console.log("session => ", session);
-  res.json({
-    user,
-    product,
-    checkoutUrl: session.url,
-  });
+    console.log("session => ", session);
+
+    db.run(
+      "INSERT INTO transactionPayment(id, fullName, orderID, status) VALUES(?, ?, ?, ?)",
+      [transactionID, name, session.id, session.status]
+    );
+
+    res.json({
+      user,
+      product,
+      checkoutUrl: session.url,
+    });
+  } catch (error) {
+    res.send(error.message);
+  }
 });
 
 app.post("/webhook", express.json({ type: "application/json" }), (req, res) => {
   const event = req.body;
 
-  // Handle the event
   switch (event.type) {
     case "checkout.session.completed":
       const paymentData = event.data.object;
       console.log("paymentData", paymentData);
+
+      db.run(
+        "UPDATE transactionPayment SET status = ?, paymentIntent = ? WHERE orderID = ?",
+        [paymentData.status, paymentData.payment_intent, paymentData.id]
+      );
       break;
     case "payment_method.attached":
       const paymentMethod = event.data.object;
@@ -72,7 +124,6 @@ app.post("/webhook", express.json({ type: "application/json" }), (req, res) => {
       console.log(`Unhandled event type ${event.type}`);
   }
 
-  // Return a response to acknowledge receipt of the event
   res.json({ received: true });
 });
 
